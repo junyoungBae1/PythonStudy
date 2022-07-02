1326ee6df70340b896654526da5dfc34f4156d81
junyoungBae1 <bjy556@gmail.com>
2022-07-03 00:39:50 +0900

    0702

--- Silver/1912연속합.py
+++ Silver/1912연속합.py
@@ -1,23 +1,14 @@
 # 연속합(1912) https://www.acmicpc.net/problem/1912
-# 다이나믹 프로그래밍
+# DP
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
 arr = list(map(int, input().split()))
-d = [arr[0], arr[0]]
+d = [0] * n
+d[0] = arr[0]
+for i in range(1, n):
+    d[i] = max(d[i-1]+arr[i], arr[i])
 
-for i in range(1,n-1):
-    if arr[i] >= 0:
-        d[0] += arr[i]
-        d[1] += arr[i]
-    else:
-        if d[1] > d[0]:
-            d[0] = d[1]
-        else:
-            d[0] += arr[i]
-        if d[i] < 0:
-            continue
-        else:
-            d[i] = 0
-print(d[0])
+print(max(d))
