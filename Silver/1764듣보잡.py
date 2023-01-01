79e7d5e2a6d73959f7c8cc51fea90f567116b262
Bae <bjy556@gmail.com>
2023-01-01 20:39:04 +0900

    20230101

---
+++ Silver/1764듣보잡.py
@@ -0,0 +1,19 @@
+# 1764 듣보잡 https://www.acmicpc.net/problem/1764
+# 교집합 &, 합집합 |, .union, 차집합 -,.difference
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+nset = set()
+mset = set()
+for _ in range(n):
+    nset.add(input())
+for _ in range(m):
+    mset.add(input())
+
+mn = sorted(list(nset & mset))
+print(len(mn))
+
+for i in mn:
+    print(i)
