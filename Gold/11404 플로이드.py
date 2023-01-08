db1ad38c380c736b886308fb74cc09c69fddd2f0
Bae <bjy556@gmail.com>
2023-01-09 00:12:02 +0900

    20230108

---
+++ Gold/11404 플로이드.py
@@ -0,0 +1,31 @@
+# 11404 플로이드 https://www.acmicpc.net/problem/11404
+# 플로이드–워셜
+import sys
+
+input = sys.stdin.readline
+INF = 1e5 + 1
+# 도시 개수
+n = int(input())
+# 버스 개수
+m = int(input())
+
+bus = [[INF for _ in range(n + 1)] for _ in range(n + 1)]
+for i in range(m):
+    a, b, c = map(int, input().split())
+    bus[a][b] = min(c, bus[a][b])
+# 플로이드 워셜
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if i == j:
+                bus[i][j] == 0
+            else:
+                bus[i][j] = min(bus[i][j], bus[i][k] + bus[k][j])
+
+for i in bus[1:]:
+    for j in i[1:]:
+        if j == INF:
+            print(0, end=" ")
+        else:
+            print(j, end=" ")
+    print()
