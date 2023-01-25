9c6c15950746c8411cacaf65a98ca5b08462fda2
Bae <bjy556@gmail.com>
2023-01-25 23:18:28 +0900

    stack

---
+++ Silver/1918 후위 표기식.py
@@ -0,0 +1,29 @@
+#1918 후위표기식 https://www.acmicpc.net/problem/1918
+
+a = input()
+stack = [] #스택
+res='' #출력
+
+for x in a:
+    if x.isalpha(): #피연산자인지 아닌지 확인
+        res+=x
+    else:
+        if x == '(':
+            stack.append(x)
+        elif x == '*' or x =='/':
+            while stack and (stack[-1]=='*' or stack[-1]=='/'):
+                res+=stack.pop()
+            stack.append(x)
+        elif x == '+' or x == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(x)
+        elif x == ')':
+            while stack and stack[-1] != '(':
+                res+=stack.pop()
+            stack.pop()
+
+#스택안에 남아있는 값들 pop
+while stack:
+    res += stack.pop()
+print(res)
