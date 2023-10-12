477beb82e85c82b2152fe6af71ffb4144ce98f9d
Bae <bjy556@gmail.com>
2023-10-12 14:15:17 +0900

    완전탐색

--- SSAFY_SWEA/D3_1244_최대상금.py
+++ SSAFY_SWEA/D3_1244_최대상금.py
@@ -1,39 +1,22 @@
 # 1244
 
 
-T = int(input())
-for test_case in range(1, T + 1):
-    answer = -1
-    value, cnt = input().split()
-    n = len(value)
-    cnt = int(cnt)
-    now = {value}
-    nxt = set()
-    for _ in range(n):
-        while now:
-            s = now.pop()
-            s = list(s)
-            for i in range(n):
-                for j in range(i + 1, n):
-                    s[i], s[j] = s[j], s[i]
-                    nxt.add(''.join(s))
-                    s[i], s[j] = s[j], s[i]
-        now, nxt = nxt, now
-    answer = max(map(int, now))
-    print("#{} {}".format(test_case, answer))
-
 for tc in range(1,int(input())+1):
     data, K = input().split() # 숫자판의 정보, 교환횟수
     K = int(K)
     N = len(data)
     now = set([data])
+
     nxt = set()
     for _ in range(K):
+
         while now:
+            print(now)
             s = now.pop()
             s = list(s)
             for i in range(N):
                 for j in range(i+1,N):
+                    print("s",s,"nxt",nxt)
                     s[i],s[j] = s[j],s[i]
                     nxt.add(''.join(s))
                     s[i], s[j] = s[j], s[i]
