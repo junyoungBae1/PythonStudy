6e33b049e90409f96d146f7f35af302047f8c719
Bae <bjy556@gmail.com>
2024-03-11 21:58:09 +0900

    dp

--- Silver/11057 오르막 수.py
+++ Silver/11057 오르막 수.py
@@ -1,16 +1,30 @@
 # 11057 오르막 수
 
-import sys
-
-input = sys.stdin.readline
-
+# import sys
+#
+# input = sys.stdin.readline
+#
+# n = int(input())
+#
+# dp = [1 for _ in range(10)]
+#
+# for i in range(n - 1):
+#     for j in range(1, 10):
+#         dp[j] += dp[j - 1]
+#         print(dp)
+#
+# print(sum(dp) % 10007)
 n = int(input())
+dp = [[0]*10 for _ in range(n+1)]
+mod = 10007
 
-dp = [1 for _ in range(10)]
+for i in range(10):
+    dp[1][i] = 1
 
-for i in range(n - 1):
-    for j in range(1, 10):
-        dp[j] += dp[j - 1]
-        print(dp)
+for i in range(2, n+1):
+    for j in range(10):
+        for k in range(j+1):
+            dp[i][j] += dp[i-1][k]
+        dp[i][j] %= mod
 
-print(sum(dp) % 10007)
+print(sum(dp[n]) % mod)
